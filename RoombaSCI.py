a796b1d36e0df491c71a1ad0ccc4ae5a840124e3
xtruan <github@xtruan.com>
2016-03-09 00:18:43 -0500

    reveritng

--- RoombaSCI.py
+++ RoombaSCI.py
@@ -575,7 +575,7 @@ if __name__ == "__main__":
         port = sys.argv[1]
 
     if (len(sys.argv) < 3):
-        baudrate = 19200
+        baudrate = 115200
     else:
         baudrate = int(sys.argv[2])
 
